f69f86d3e9091f208849d6c866122b29c17504d6
Hamza Zaman <hamzazaman04@gmail.com>
2023-01-05 20:15:37 +0000

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,9 +1,9 @@
 sudo apt -y install git python git 
-git clone https://github.com/bezkoder/django-rest-api.git
+git clone https://github.com/Hamza-Zaman/django-rest-framework-crud.git
 cd django-rest-api
 python -m venv venv
 source venv/bin/activate
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirement.txt
+python3 -m pip install -r requirements.txt
 python3 manage.py runserver
 
